df6e650c3adef170e6672db3ba29c5f1ebc7f3d6
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-11 16:23:20 -0400

    added MyWelcomeFunction

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -2,9 +2,17 @@
 
 #include <iostream>
 
+void MyWelcomeFunction()
+{
+	std::cout << "Hey, Welcome to the course!" << std::endl;
+}
+
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+	MyWelcomeFunction();
 }
 
 
